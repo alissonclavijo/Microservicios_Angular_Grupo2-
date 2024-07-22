b68ce6373da531528b0d13a8d39bac2ae1a4c8d0
Miguel Saa <93295342+TarimaX@users.noreply.github.com>
2024-07-21 23:08:29 -0500

    Test Karma

--- Proyecto/FRONTEND/src/app/services/enrollment.service.spec.ts
+++ Proyecto/FRONTEND/src/app/services/enrollment.service.spec.ts
@@ -1,16 +1,30 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { EnrollmentService } from './enrollment.service';
+import { Course } from '../models/course.model';
+import { User } from '../models/user.model';
+import { environment } from '../enviroments/enviroment';
 
 describe('EnrollmentService', () => {
   let service: EnrollmentService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [EnrollmentService]
+    });
     service = TestBed.inject(EnrollmentService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  // Similar tests can be written for other methods (getUsers, enroll, etc.)
 });
