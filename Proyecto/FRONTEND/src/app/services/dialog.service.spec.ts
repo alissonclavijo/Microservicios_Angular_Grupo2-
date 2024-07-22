7ebe464e9e34ce421aea703e8caac95d8174b6d5
Miguel Saa <93295342+TarimaX@users.noreply.github.com>
2024-07-21 23:13:18 -0500

    Test Karma

--- Proyecto/FRONTEND/src/app/services/dialog.service.spec.ts
+++ Proyecto/FRONTEND/src/app/services/dialog.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
-import { DialogService } from './dialog.service';
+import { DialogService, DialogOptions } from './dialog.service';
 
 describe('DialogService', () => {
   let service: DialogService;
@@ -12,5 +11,30 @@ describe('DialogService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should show dialog', () => {
+    const options: DialogOptions = { message: 'Test Message' };
+    service.show(options);
+
+    service.onShow().subscribe(res => {
+      expect(res).toEqual(options);
+    });
+  });
+
+  it('should cancel dialog', () => {
+    service.cancel();
+
+    service.onCancel().subscribe(() => {
+      expect(true).toBe(true);
+    });
+  });
+
+  it('should confirm dialog', () => {
+    service.confirm();
+
+    service.onConfirm().subscribe(() => {
+      expect(true).toBe(true);
+    });
   });
 });
