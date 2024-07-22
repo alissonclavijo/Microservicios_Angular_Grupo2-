95c59889e5dcbe89d2586bfd7364d5f78733801c
Miguel Saa <93295342+TarimaX@users.noreply.github.com>
2024-07-21 23:11:01 -0500

    Test Karma

--- Proyecto/FRONTEND/src/app/services/course.service.spec.ts
+++ Proyecto/FRONTEND/src/app/services/course.service.spec.ts
@@ -1,16 +1,29 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CourseService } from './course.service';
+import { Course } from '../models/course.model';
+import { environment } from '../enviroments/enviroment';
 
 describe('CourseService', () => {
   let service: CourseService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CourseService]
+    });
     service = TestBed.inject(CourseService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  
 });
