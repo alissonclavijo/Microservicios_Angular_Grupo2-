3d94c403dbcdec89f9e14a4044b1ec6a87b455ef
Miguel Saa <93295342+TarimaX@users.noreply.github.com>
2024-07-21 23:09:40 -0500

    Test Karma

--- Proyecto/FRONTEND/src/app/services/user.service.spec.ts
+++ Proyecto/FRONTEND/src/app/services/user.service.spec.ts
@@ -1,16 +1,45 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserService } from './user.service';
+import { User } from '../models/user.model';
+import { environment } from '../enviroments/enviroment';
 
 describe('UserService', () => {
-    let service: UserService;
+  let service: UserService;
+  let httpMock: HttpTestingController;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(UserService);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
+    service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should retrieve users from the API via GET', () => {
+    const dummyUsers: User[] = [
+      { id: 1, name: 'User 1', email: 'user1@example.com' },
+      { id: 2, name: 'User 2', email: 'user2@example.com' }
+    ];
+
+    service.getUsers().subscribe(users => {
+      expect(users.length).toBe(2);
+      expect(users).toEqual(dummyUsers);
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
-    });
+    const req = httpMock.expectOne(`${environment.apiUrlUsers}/users`);
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyUsers);
+  });
+
+  
 });
